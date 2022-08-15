381e763732d15eeba3d2e7befbf8f9a6f8205f3a
Habip ELİŞ <habibelis65@gmail.com>
2022-08-15 12:29:48 +0300

    #modelse logo ekleme database

--- metafiksweb/anasayfa/models.py
+++ metafiksweb/anasayfa/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class sayfa_logosu(models.Model):
+    sayfa_logo = models.FileField(upload_to='logo/',blank = True,null = True,verbose_name="Sayfaya Logo Ekleyin")
