31134ac7524211df9c555e8922492f797bb5d2b5
Habip ELİŞ <habibelis65@gmail.com>
2022-08-15 12:32:08 +0300

    #adres ve numara modelse eklenmesi

--- metafiksweb/anasayfa/models.py
+++ metafiksweb/anasayfa/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class sayfa_logosu(models.Model):
     sayfa_logo = models.FileField(upload_to='logo/',blank = True,null = True,verbose_name="Sayfaya Logo Ekleyin")
+
+class numara(models.Model):
+    sirket_numarasi = models.CharField(max_length=11)
+class adres(models.Model):
+    sirket_adresi = models.CharField(max_length=200)
